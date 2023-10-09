4d0fb418f5d33e9903efd4f1c770bc8624969124
melissa-24 <mlongenberger@codingdojo.com>
2023-10-09 14:14:29 -0400

    updated settings

--- server/fourSale/urls.py
+++ server/fourSale/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from userApp import views as app_views
+from storeApp import views as app_views
+from orderApp import views as app_views
+from themesApp import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('storeApp.urls')),
+    path('user/', include('userApp.urls')),
+    path('order/', include('orderApp.urls')),
+    path('themes.', include('themesApp.urls'))
 ]
