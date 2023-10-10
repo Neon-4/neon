39e4a50c10d92fc28cf3a9cd70eeb47abf6f8b2f
Harrison Anthony <harrisoneanthony@gmail.com>
2023-10-10 11:05:18 -0400

    adding recoil state and login modal

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '@/components/Landing'
 import Navbar from '@/components/Navbar'
+import Login from '@/components/Login'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Landing/>
     </div>
   )
