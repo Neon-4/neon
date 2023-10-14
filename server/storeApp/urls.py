f8b9e960309f8f26c98b3b393343b6d022787f35
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-10-14 02:27:38 -0400

    added more routes

--- server/storeApp/urls.py
+++ server/storeApp/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('categories/', views.apiGetCategory),
+    path('products/', views.apiGetProduct),
+    path('product/<int:prod_id>/view/', views.apiGetOneProd),
+    path('category/<int:category_id>/products/', views.apiGetProdByCat)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/storeApp/views.py
+++ server/storeApp/views.py
@@ -13,3 +13,25 @@ from rest_framework.response import Response
 def apiGetCategory(request):
     categories = list(Category.objects.all().values())
     return JsonResponse(categories, safe=False, content_type="application.json")
+
+@api_view(['GET'])
+def apiGetProduct(request):
+    products = list(Product.objects.all().values())
+    return JsonResponse(products, safe=False, content_type="application.json")
+
+@api_view(['GET'])
+def apiGetProdByCat(request, category_id):
+    prodByCat = list(Product.objects.filter(category_id=category_id))
+    return JsonResponse(prodByCat, safe=False, content_type="application.json")
+
+@api_view(['GET'])
+def apiGetOneProd(request, prod_id):
+    product = Product.objects.get(id=prod_id)
+    prodImages = ProductImages.objects.filter(prod_id=prod_id)
+    data = [product, prodImages]
+    return JsonResponse(data, safe=False, content_type="application.json")
+
+@api_view(['GET'])
+def apiGetProdImages(request):
+    prodImages = list(ProductImages.objects.all().values())
+    return JsonResponse(prodImages, safe=False, content_type="application.json")
