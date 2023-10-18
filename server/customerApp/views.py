242eaabe85c0c501a54037dab5f80bc9fd9306d0
melissa-24 <mlongenberger@codingdojo.com>
2023-10-18 18:46:51 -0400

    saving reg route

--- server/customerApp/views.py
+++ server/customerApp/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
+import bcrypt
 
 @api_view(['GET'])
 def apiGetAllCustomers(request):
@@ -13,3 +14,23 @@ def apiGetAllCustomers(request):
     except Customer.DoesNotExist:
         data = []
         return Response(data, status=status.HTTP_404_NOT_FOUND)
+    
+
+@api_view(['POST'])
+def apiCustomerRegistration(request):
+    if request.method == 'POST':
+        print("going into reg request.data", request.data)
+        errors = Customer.objects.validate(request.data)
+        if errors:
+            print("in reg found errors")
+            for err in errors.values(request.data):
+                messages = messages.error(request, err)
+                print('reg errors for looo', messages)
+            return Response(messages, status=status.HTTP_404_NOT_FOUND)
+        hashedPw = bcrypt.hashpw(request.data['password'].encode(), bcrypt.gensalt()).decode()
+        request.data['password'] = hashedPw
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
