15f79fb1831d0139f9037fba37380affb2aa99c0
Christopher J <chrisbjones1987@yahoo.com>
2023-10-22 14:31:46 -0500

    Added Images

--- client/api/products.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { axios } from "axios";
-
-export const products = async () => {
-  const response = await axios.get("https://ecom-back.thehive-services.com/api/store/products/");
-  return response.data;
-}
--- client/components/products/allProducts.jsx
+++ client/components/products/allProducts.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { products } from '@/api/products';
 import ItemBox from '../ItemsBox';
 import AddToCartButton from '../AddToCartButton';
 
@@ -40,8 +39,6 @@ const AllProducts = () => {
             setStartIndex(startIndex - 5);
         }
     };
-    // This is how the image src will need to be linked in but this errors out
-    // var imgSrc=`https://ecom-back.thehive-services.com${product.image}`
     return (
         <div>
             <div className="flex items-center justify-between mb-4">
@@ -55,7 +52,7 @@ const AllProducts = () => {
             <div className="overflow-x-auto flex pb-4">
                 {allProd.map(product => (
                     <div className="bg-[#eae7e7] rounded-lg p-4 shadow-md mx-2" key={product.id}>
-                        <img src={imgSrc} alt={product.name} className="object-cover mb-2" draggable='false' />
+                        <img src={`http://ecom-back.thehive-services.com/media/${product.image}`} alt={product.name} className="object-cover mb-2" draggable='false' />
                         <p className="text-left font-bold text-xs md:text-md lg:text-md">{product.name}</p>
                         <p className="text-left text-xs md:text-md lg:text-md mt-3">${product.price}</p>
                         <div className='mt-6'>
