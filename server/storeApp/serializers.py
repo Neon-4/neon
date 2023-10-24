4fc5b7558eabc0309467e881fc6bec05ac177f15
melissa-24 <mlongenberger@codingdojo.com>
2023-10-23 21:11:35 -0400

    updated get one image link

--- server/storeApp/serializers.py
+++ server/storeApp/serializers.py
@@ -21,6 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductImagesSerializer(serializers.ModelSerializer):
+    image_name = serializers.SerializerMethodField()
+
+    def get_image_name(self, obj):
+        if obj.image:
+            return f"https://ecomm-back.thehive-services.com/media/{obj.image}"
+
     class Meta:
         model = ProductImages
         fields = '__all__'
